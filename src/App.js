de09b12fb9ad0c26f33dab5230e2617aeee1bd40
skygl <jupiter9501@gmail.com>
2020-06-24 00:16:37 +0900

    feat: BM JUA 폰트 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from "./templates/header";
 import './App.css';
+import './styles/css/fonts.css';
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
 }
 
 export default App;

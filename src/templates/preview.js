900054b7ebcaf7b3972c89182249cbd61e2576c5
skygl <jupiter9501@gmail.com>
2020-07-06 13:20:05 +0900

    feat: API 서버의 프록시 기능을 사용하도록 수정 및 useEffect 로직 파일 분리

--- src/templates/preview.js
+++ src/templates/preview.js
@@ -1,41 +1,5 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-
-const proxyPrefix = "https://cors-anywhere.herokuapp.com/";
-const regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
-const urlValidator = new RegExp(regexQuery, "i");
-
-const parseImage = (html) => {
-    let element;
-    if (!!(element = html.querySelector("meta[property='og:image']"))) {
-        return element.getAttribute("content");
-    }
-    return "/devlog.png";
-};
-
-const parseTitle = (html) => {
-    let element;
-    if (!!(element = html.querySelector("meta[property='og:title']"))
-        || !!(element = html.querySelector("meta[name='title']"))) {
-        return element.getAttribute("content");
-    } else if (!!(element = html.querySelector("head > title"))) {
-        return element.innerHTML;
-    } else {
-        return "";
-    }
-};
-
-const parseDescription = (html) => {
-    let element;
-    if (!!(element = html.querySelector("meta[property='og:description']"))
-        || !!(element = html.querySelector("meta[name='description']"))) {
-        return element.getAttribute("content");
-    } else if (!!(element = html.querySelector("head > description"))) {
-        return element.innerHTML;
-    } else {
-        return "";
-    }
-};
+import React, {useState} from "react";
+import {useProxy} from "../useApi";
 
 const Preview = ({url}) => {
 
@@ -46,43 +10,7 @@ const Preview = ({url}) => {
         image: ""
     });
 
-    useEffect(() => {
-        if (urlValidator.test(url)) {
-            axios.get(proxyPrefix + url)
-                .then(res => {
-                    const html = document.createElement('html');
-                    html.innerHTML = res.data;
-                    const image = parseImage(html);
-                    const title = parseTitle(html);
-                    const description = parseDescription(html);
-                    setOg({
-                        url, title, description, image
-                    })
-                })
-                .catch(() => {
-                    setOg({
-                        url,
-                        title: "올바른 URL을 입력해주세요",
-                        description: "",
-                        image: "/alert.svg"
-                    })
-                })
-        } else if (url === "") {
-            setOg({
-                url,
-                title: "URL을 입력해주세요",
-                description: "",
-                image: "/devlog.png"
-            })
-        } else {
-            setOg({
-                url,
-                title: "올바른 URL을 입력해주세요",
-                description: "",
-                image: "/alert.svg"
-            })
-        }
-    }, [url]);
+    useProxy(url, setOg);
 
     const onClick = () => {
         window.open(og.url, '_blank');
--- src/useApi.js
+++ src/useApi.js
@@ -26,3 +26,84 @@ export function usePostList(date, page, offset, setCards, setHasMorePosts) {
         })
     }, [date, page, offset, setCards, setHasMorePosts]);
 }
+
+const regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
+const urlValidator = new RegExp(regexQuery, "i");
+
+const parseImage = (html) => {
+    let element;
+    if (!!(element = html.querySelector("meta[property='og:image']"))) {
+        return element.getAttribute("content");
+    }
+    return "/devlog.png";
+};
+
+const parseTitle = (html) => {
+    let element;
+    if (!!(element = html.querySelector("meta[property='og:title']"))
+        || !!(element = html.querySelector("meta[name='title']"))) {
+        return element.getAttribute("content");
+    } else if (!!(element = html.querySelector("head > title"))) {
+        return element.innerHTML;
+    } else {
+        return "";
+    }
+};
+
+const parseDescription = (html) => {
+    let element;
+    if (!!(element = html.querySelector("meta[property='og:description']"))
+        || !!(element = html.querySelector("meta[name='description']"))) {
+        return element.getAttribute("content");
+    } else if (!!(element = html.querySelector("head > description"))) {
+        return element.innerHTML;
+    } else {
+        return "";
+    }
+};
+
+export function useProxy(url, setOg) {
+    useEffect(() => {
+        if (urlValidator.test(url)) {
+            axios({
+                method: 'GET',
+                url: `http://${API_SERVER_HOST}:${API_SERVER_PORT}/proxy`,
+                params: {
+                    url: url
+                }
+            })
+                .then(res => {
+                    const html = document.createElement('html');
+                    html.innerHTML = res.data;
+                    const image = parseImage(html);
+                    const title = parseTitle(html);
+                    const description = parseDescription(html);
+                    setOg({
+                        url, title, description, image
+                    })
+                })
+                .catch(() => {
+                    setOg({
+                        url,
+                        title: "올바른 URL을 입력해주세요",
+                        description: "",
+                        image: "/alert.svg"
+                    })
+                })
+        } else if (url === "") {
+            setOg({
+                url,
+                title: "URL을 입력해주세요",
+                description: "",
+                image: "/devlog.png"
+            })
+        } else {
+            setOg({
+                url,
+                title: "올바른 URL을 입력해주세요",
+                description: "",
+                image: "/alert.svg"
+            })
+        }
+    }, [url, setOg]);
+}

2f32aba2681867d564c3aad88c44771fa8462091
skygl <jupiter9501@gmail.com>
2020-07-08 13:54:48 +0900

    feat: Card에 URL 링크 추가

--- src/components/molecules/card.js
+++ src/components/molecules/card.js
@@ -4,12 +4,14 @@ import CardImage from "../atoms/cardImage";
 import CardDescription from "../atoms/cardDescription";
 
 const Card = ({card}) => {
-    const {title, tags, imageUrl, description} = card;
+    const {title, tags, imageUrl, description, url} = card;
 
     return (
         <div className={"card"}>
-            <CardImage image_url={imageUrl}/>
-            <CardDescription title={title} tagNames={tags} description={description}/>
+            <a href={url} target={"_blank"}>
+                <CardImage image_url={imageUrl}/>
+                <CardDescription title={title} tagNames={tags} description={description}/>
+            </a>
         </div>
     )
 };
